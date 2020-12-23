0a7b905f1e17a72fc3c4a6f3893b5ec14eeef6da
trixt3r7 <68664129+trixt3r7@users.noreply.github.com>
2020-12-23 20:27:41 +0100

    Update to Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -4,22 +4,42 @@ namespace gik299_project
 {
     class Menu
     {
+        // Color Scheme
+        public void PriColor()
+        {
+            // Primary Color
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+        public void SecColor()
+        {
+            // Secondary Color
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        public void TerColor()
+        {
+            // Tertiary Color
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+        }
+        public void ResetColor()
+        {
+            Console.ResetColor();
+        }
 
         public void GameLogo()
         {
-            Console.WriteLine(@"
-                 ______      __                    
-                / ____/_  __/ /_  ___  _____       
-               / /   / / / / __ \/ _ \/ ___/       
-              / /___/ /_/ / /_/ /  __/ /           
-      ____    \____/\__, /_.___/\___/_/            
-     / __ \__  ____/____/__  ___  ____  ____  _____
-    / / / / / / / __ \/ __ `/ _ \/ __ \/ __ \/ ___/
-   / /_/ / /_/ / / / / /_/ /  __/ /_/ / / / (__  ) 
-  /_____/\__,_/_/ /_/\__, /\___/\____/_/ /_/____/  
-                    /____/");
+            PriColor(); Console.WriteLine("                         ______      __");
+            SecColor(); Console.Write("■■■■■■■■■■■■■■■■■■■■■"); PriColor(); Console.Write(@"   / ____/_  __/ /_  ___  _____       "); SecColor(); Console.WriteLine("■■■■■■■■■■■■■■");
+            PriColor(); Console.Write("■■■■■■■■■■■■■■■■■■■"); PriColor(); Console.Write(@"    / /   / / / / __ \/ _ \/ ___/     "); PriColor(); Console.WriteLine("■■■■■■■■■■■■■■■■");
+            SecColor(); Console.Write("■■■■■■■■■■■■■■■■■"); PriColor(); Console.Write(@"     / /___/ /_/ / /_/ /  __/ /      "); SecColor(); Console.WriteLine("■■■■■■■■■■■■■■■■■■■");
+            PriColor(); Console.Write("■■■■■■■■■■■■"); SecColor(); Console.Write(@"  ____"); PriColor(); Console.WriteLine(@"    \____/\__, /_.___/\___/_/     ■■■■■■■■■■■■■■■■■■■■■");
+            SecColor(); Console.Write("■■■■■■■■■■"); SecColor(); Console.Write(@"   / __ \__  ____"); PriColor(); Console.Write(@"/____/");
+            SecColor(); Console.WriteLine(@"__  ___  ____  ____  _____      ■■■■■■■■");
+            PriColor(); Console.Write("■■■■■■■■"); SecColor(); Console.Write(@"    / / / / / / / __ \/ __ `/ _ \/ __ \/ __ \/ ___/     "); PriColor(); Console.WriteLine("■■■■■■■■■");
+            SecColor(); Console.Write("■■■■■■"); SecColor(); Console.Write(@"     / /_/ / /_/ / / / / /_/ /  __/ /_/ / / / (__  )    "); SecColor(); Console.WriteLine("■■■■■■■■■■■");
+            PriColor(); Console.Write("■■■■"); SecColor(); Console.Write(@"      /_____/\__,_/_/ /_/\__, /\___/\____/_/ /_/____/  "); PriColor(); Console.WriteLine("■■■■■■■■■■■■■■");
+            SecColor(); Console.WriteLine("                            /____/");
+            ResetColor();
         }
-
         public void WelcomeText()
         {
 
